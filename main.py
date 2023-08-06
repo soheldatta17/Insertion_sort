43f19f8c1f2bbc7c4c9fb4b57630e10c11cddf6f
soheldatta17 <soheldatta17@gmail.com>
2023-08-06 14:00:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def insert(a):
+    for i in range(1,len(a)):
+        key=a[i]
+        j=i-1
+        while j>=0 and a[j]>key:
+            a[j+1]=a[j]
+            j-=1
+        a[j+1]=key
+    return a
+a=[12,54,65,7,23,9]
+print(insert(a))
